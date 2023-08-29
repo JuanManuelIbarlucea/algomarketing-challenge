5b0375ae205f9de8c04ef4758e5765c69eed8791
JuanManuelIbarlucea <jmibarlucea@gmail.com>
2023-08-29 01:56:58 -0300

    Added TaskDisplay component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import TaskDisplay from '@/components/task-display';
 import TaskForm from '@/components/task-form';
 
 export default function Home() {
   return (
     <main className="flex gap-5 w-full flex-col md:flex-row justify-center items-center h-[40rem]">
       <TaskForm />
+      <TaskDisplay />
     </main>
   );
 }
---
+++ components/task-component.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function TaskComponent() {
+  return <div>TaskComponent</div>;
+}
