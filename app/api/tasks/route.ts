9d6d1c3a6a4a0f907c3bd7015e4057e2ffcd5b8e
JuanManuelIbarlucea <jmibarlucea@gmail.com>
2023-08-29 06:30:48 -0300

    You can now  delete a Task

--- app/api/tasks/route.ts
+++ app/api/tasks/route.ts
@@ -2,7 +2,7 @@
 
 import { mongooseConnect } from '@/lib/mongoose';
 import { Task } from '@/models/Task';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
   await mongooseConnect();
@@ -22,7 +22,14 @@ export async function POST(request: Request) {
   await mongooseConnect();
 
   const body = await request.json();
-  console.log(body);
   const productDoc = await Task.create(body);
   return NextResponse.json(productDoc);
+}
+
+export async function DELETE(request: NextRequest) {
+  await mongooseConnect();
+
+  const id = request.nextUrl.searchParams.get('id');
+  await Task.findByIdAndDelete(id);
+  return new Response('Task deleted');
 }
--- components/task-card.tsx
+++ components/task-card.tsx
@@ -1,4 +1,6 @@
+import useTasksContext from '@/contexts/tasks-context';
 import { LabelsType, Task, TaskType } from '@/lib/types';
+import axios from 'axios';
 import { twMerge } from 'tailwind-merge';
 
 export default function TaskCard({ task }: { task: Task }) {
@@ -19,6 +21,13 @@ export default function TaskCard({ task }: { task: Task }) {
   }
 
   const dueDate = new Date(task.dueDate);
+  const { setTasks } = useTasksContext();
+
+  async function deleteTask() {
+    setTasks((prev) => prev.filter((t) => t._id !== task._id));
+    await axios.delete(`/api/tasks?id=${task._id}`);
+  }
+
   return (
     <div className="bg-white rounded-xl p-5 text-ellipsis shadow-xl relative overflow-hidden">
       <div className="flex items-center">
@@ -46,14 +55,35 @@ export default function TaskCard({ task }: { task: Task }) {
       </p>
       <p className="text-xl font-bold uppercase break-word">{task.name}</p>
       <p className="text-sm break-words">{task.description}</p>
-      <p
+
+      <div
+        className="absolute bg-red-500  rounded-tr-xl p-2 top-0 right-0"
+        onClick={deleteTask}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+          />
+        </svg>
+      </div>
+
+      <div
         className={twMerge(
-          'text-white absolute p-2 top-[2rem] -right-[7rem] rotate-45 bg-red-700 w-[20rem] text-center text-sm',
+          'text-white absolute p-2 top-[2rem] -right-[7rem] rotate-45 bg-red-700 w-[20rem] text-center text-sm overflow-hidden',
           getLabelColor(task.label)
         )}
       >
         {task.label}
-      </p>
+      </div>
     </div>
   );
 }
